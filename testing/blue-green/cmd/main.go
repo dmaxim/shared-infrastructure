89963e53452d82c95299c679907dc39c526afd97
Dan Maxim <dmaxim@mxinformatics.com>
2024-12-20 14:14:08 -0500

    Test app

--- testing/blue-green/cmd/main.go
+++ testing/blue-green/cmd/main.go
@@ -9,10 +9,10 @@ import (
 var BuildVersion = "v0.0"
 
 func main() {
-	log.Println("Starting server...")
+	log.Println("Starting server...2.0")
 	http.HandleFunc("/version", version)
 	http.ListenAndServe(":3000", nil) //nolint
-	
+
 }
 
 func version(w http.ResponseWriter, r *http.Request) {
