4bcd596ef5407148c4fa67a662746b6983dac2d2
Dan Maxim <dmaxim@mxinformatics.com>
2026-04-01 14:43:01 -0400

    test with traefik

--- traefik/install.sh
+++ traefik/install.sh
@@ -8,3 +8,11 @@ helm install traefik traefik/traefik --namespace traefik --create-namespace -f .
 k describe GatewayClass traefik
 
 k describe Gateway traefik -n traefik
+
+helm plugin install https://github.com/databus23/helm-diff
+
+helm diff upgrade traefik traefik/traefik --namespace traefik -f ./values.yaml --version 38.0.1
+
+helm upgrade traefik traefik/traefik --namespace traefik -f ./values.yaml --version 38.0.1
+
+k create secret tls traefik-tls-cert --cert=./STAR_danmaxim_net.pem --key=./STAR_danmaxim_net.key -n traefik
